addea9c1fd0062fb78abb65aa6257bff98c568d8
jeyce <jeyce@jeyce.com>
2018-07-19 20:02:54 -0300

    alterando backoffice 2

--- app/controllers/backoffice/categories_controller.rb
+++ app/controllers/backoffice/categories_controller.rb
@@ -4,4 +4,22 @@ class Backoffice::CategoriesController < BackofficeController
   def index
     @categories = Category.all
   end
+
+  def new
+  	
+  end
+
+  def create
+  	
+  end
+
+
+  def edit
+  	
+  end
+
+  def update
+  	
+  end
+
 end
