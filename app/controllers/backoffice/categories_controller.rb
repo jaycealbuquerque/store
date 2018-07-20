274f7dd93ad745621085e3a2f58b8ce3a0d77d17
jeyce <jeyce@jeyce.com>
2018-07-19 21:41:32 -0300

    alterando backoffice 3

--- app/controllers/backoffice/categories_controller.rb
+++ app/controllers/backoffice/categories_controller.rb
@@ -6,11 +6,16 @@ class Backoffice::CategoriesController < BackofficeController
   end
 
   def new
-  	
+  	@category = Category.new
   end
 
   def create
-  	
+  	@category = Category.new(params_category)
+    if @category.save
+      redirect_to backoffice_categories_path, notice: "a categoria (#{@category.description}) foi cadastrada"
+    else
+      render :new
+    end  
   end
 
 
@@ -20,6 +25,10 @@ class Backoffice::CategoriesController < BackofficeController
 
   def update
   	
+  end
+
+  def params_category
+    params.require(:category).permit(:description)
   end
 
 end
