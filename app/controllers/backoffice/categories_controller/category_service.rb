f1b997abdab267112a92454969eaabdc29d8237e
jeyce987 <jeyce_jp@hotmail.com>
2018-07-31 22:15:27 -0300

    POROs

---
+++ app/controllers/backoffice/categories_controller/category_service.rb
@@ -0,0 +1,14 @@
+class Backoffice::CategoriesController::CategoryService
+	attr_accessor :category
+
+	def self.create(parems_category)
+		@category = Category.new(parems_category)
+
+		if @category.valid?
+			@category.save
+			
+		end
+		@category
+	end
+
+end
