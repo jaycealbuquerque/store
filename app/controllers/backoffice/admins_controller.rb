6aef1a430401435941e83fc46727a8e226ef57fa
jeyce987 <jeyce_jp@hotmail.com>
2018-08-07 22:52:07 -0300

    autorizando a role no controller

--- app/controllers/backoffice/admins_controller.rb
+++ app/controllers/backoffice/admins_controller.rb
@@ -58,7 +58,7 @@
     end
     
     def params_admin
-      params.require(:admin).permit(:name, :email, :password, :password_confirmation)
+      params.require(:admin).permit(:name, :email,:role, :password, :password_confirmation)
     end
 
 
