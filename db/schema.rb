375c1fe4ec5f7cfdcf706b3c243d1732fa6a3c60
jeyce987 <jeyce_jp@hotmail.com>
2018-08-20 19:56:31 -0300

    justando o preço no ads

--- db/schema.rb
+++ db/schema.rb
@@ -38,9 +38,9 @@ ActiveRecord::Schema.define(version: 20180820222635) do
     t.text     "description"
     t.integer  "member_id"
     t.integer  "category_id"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.integer  "price_cents",             default: 0
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.integer  "price_cents"
   end
 
   add_index "ads", ["category_id"], name: "index_ads_on_category_id"
