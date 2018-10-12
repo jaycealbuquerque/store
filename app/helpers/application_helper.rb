517c8c948b12dfcfc958f505532a863b3f78b62d
jeyce987 <jeyce_jp@hotmail.com>
2018-10-12 13:18:26 -0300

    unificando o cadastro e o login do devise

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,11 @@ def current_user
   current_member
 end  
 
+def devise_sign_in?
+    params[:controller] == 'members/sessions' ? '' : 'display:none;'
+  end
+   def devise_sign_up?
+    params[:controller] == 'members/registrations' ? '' : 'display:none;'
+  end
+
 end
