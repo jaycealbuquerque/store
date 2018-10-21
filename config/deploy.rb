e59f26626f1416bcb8a20125c5aaa144b8e23305
jeyce987 <jeyce_jp@hotmail.com>
2018-10-21 18:37:56 -0300

    ajustes

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,6 @@ set :repo_url, "https://github.com/jeyce987/store.git"
 # Default deploy_to directory is /var/www/my_app_name
  set :deploy_to, "/var/www/loja"
 
- set :scm, :git
  set :branch,"master"
  
 # Default value for :format is :airbrussh.
